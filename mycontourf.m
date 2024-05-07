e67dbbe86c9f9e780a68071caee7feaf66e792c4
Naimba <150248754+Naimba@users.noreply.github.com>
2024-05-07 15:13:09 +0800

    Update mycontourf.m

--- mycontourf.m
+++ mycontourf.m
@@ -1,5 +1,5 @@
 function [M,c,Z1] = mycontourf(X,Y,Z,levels,cm,mode)
-% [M,c] = MYCONTOURF(X,Y,Z,levels,cm)
+% [M,c] = MYCONTOURF(X,Y,Z,levels,cm,mode)
 % 本函数将配合mycolorbar函数实现不等间距等值线填色
 % 注意：对于不等间距的格点插值是线性插值
 % -----------------------输入变量说明----------------------- 
@@ -14,9 +14,9 @@ function [M,c,Z1] = mycontourf(X,Y,Z,levels,cm,mode)
 % c：等高线对象，显示等高线图后，使用c设置属性。
 %
 % -----------------------函数调用示例----------------------- 
-% [M,c,Z1] = mycontourf(X,Y,u',-25:5:25,cm,'m');
+% [M,c,Z1] = MYCONTOURF(X,Y,u',-25:5:25,cm,'m');
 % 
-% [M,c,Z1] = mycontourf(X,Y,u',[-10 -8 -6 -4 -2 0 2 10 15 20 40],cm,'m');
+% [M,c,Z1] = MYCONTOURF(X,Y,u',[-10 -8 -6 -4 -2 0 2 10 15 20 40],cm,'m');
 % 
 %  This fuction is made by Yifan Liu, Academy of the Future Ocean, Ocean University of China.
 
